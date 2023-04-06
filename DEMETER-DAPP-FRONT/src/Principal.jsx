1031f69d4b64b801ea22c951f88476b5120bfdc7
CodeSistency <valero.jozee@gmail.com>
2023-04-05 22:37:33 -0400

    creación de proyecto Demeter Front-end

---
+++ DEMETER-DAPP-FRONT/src/Principal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Principal() {
+  return (
+    <div style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid red"
+    }}>principal</div>
+  )
+}
+
+export default Principal
---
+++ DEMETER-DAPP-FRONT/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='logo'>
+        <img className='logo-imagen' src='logo.png'/>
+        <div className='logo-descripcion'>
+            <h3 className='titulo-logo'>Programa Deméter</h3>
+            <h4 className='fundacoven'>Powered By Fundacoven</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Logo
